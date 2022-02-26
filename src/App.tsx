7c939eeadb252a5145c9b39f1b2c2dffae6ceac1
Subrata34 <subratamodak3491@gmail.com>
2022-02-26 23:55:14 +0600

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useCallback, useReducer, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, TextField } from '@mui/material';
+
+interface Todo{
+    id:number,
+    text:string
+}
+type Actiontype=
+   |{type:"ADD",text:string} 
+   |{type:"REMOVE"; id:number};
 
 function App() {
+  function reducer(state:Todo[],action:Actiontype){
+    switch(action.type){
+      case "ADD" :
+        return[
+          ...state,
+          {
+            id:state.length,
+            text:action.text,
+          },
+        ];
+        case "REMOVE":
+          return state.filter(({id})=>id!==action.id);
+    }
+  }
+  const [todos, dispatch] = useReducer(reducer, []);
+  const newTodoRef=useRef<HTMLInputElement>(null);
+  const onaddtodo=useCallback(()=>{
+ if(newTodoRef.current){
+   dispatch({
+     type:"ADD",
+     text:newTodoRef.current.value
+   })
+   newTodoRef.current.value="";
+ }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <TextField
+  label="Input  Name"
+  variant="standard"
+  ref={newTodoRef}
+/>
+<Button variant="contained" onClick={onaddtodo}>add</Button>
+      {
+        todos.map((todo)=>(
+        <div key={todo.id}>
+          {todo.text}</div>
+        ))
+      }
+     
     </div>
   );
 }
